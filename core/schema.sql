c9840ac8f50c17cda5c46c867c393c58b240f815
ppotseluev <potseluev-pasha@yandex.ru>
2024-03-25 18:45:30 +0700

    ITDESK-28 add prefixes to db table names

--- core/schema.sql
+++ core/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE bots_states (
+CREATE TABLE it_desk_bots_states (
   id           varchar(255) NOT NULL,
   value        varchar(255) DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE invitations(
+CREATE TABLE it_desk_invitations(
     tg_username     varchar(100)      NOT NULL,
     role            SMALLINT          NOT NULL,
     valid_until     TIMESTAMP         NOT NULL,
     PRIMARY KEY     (tg_username, role)
 );
 
-CREATE TABLE users(
+CREATE TABLE it_desk_users(
     id            BIGSERIAL      NOT NULL,
     tg_user_id    BIGINT         NOT NULL,
     role          SMALLINT       NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE users(
     CONSTRAINT Role UNIQUE (tg_user_id, role)
 );
 
-CREATE TABLE experts(
+CREATE TABLE it_desk_experts(
     user_id                           INT NOT NULL,
     name            VARCHAR(100)      DEFAULT NULL,
     description     TEXT              DEFAULT NULL,
